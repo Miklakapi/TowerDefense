d0f332a503172400e819fa6206686986a97e0431
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-01-12 00:31:57 +0100

    Update Main.cpp

--- TowerDefenseGame/TowerDefenseGame/Main.cpp
+++ TowerDefenseGame/TowerDefenseGame/Main.cpp
@@ -1,12 +1,11 @@
+#include <SFML\Audio.hpp>
 #include <SFML\Window.hpp>
 #include <SFML\System.hpp>
 #include <SFML\Graphics.hpp>
 #include "Menu.hpp"
 #include "Type.hpp"
-#include <iostream>
 
 using namespace sf;
-using namespace std;
 
 int main() {
 /////////////////////////////////////////////////////////////////////////////////////
@@ -19,14 +18,21 @@ int main() {
 /////////////////////////////////////////////////////////////////////////////////////
 
 	Texture mainMenu;
-		mainMenu.loadFromFile("img/MenuMain.png");
-	Texture options;
-	//	options.loadFromFile();
-	Texture credits;
-	//	credits.loadFromFile();
+		mainMenu.loadFromFile("Img/MenuMain.png");
+	Texture optionsMenu;
+		optionsMenu.loadFromFile("Img/MenuOptions.png");
+	Texture creditsMenu;
+		creditsMenu.loadFromFile("Img/MenuCredits.png");
 
 /////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////
+
+	SoundBuffer clickBuffer;
+	clickBuffer.loadFromFile("Sound/click.ogg");
+
+	Sound clickSound;
+	clickSound.setBuffer(clickBuffer);
+	clickSound.setVolume(5);
 
 	const Field field1{ Vector2i{974,215},Vector2i{1095,259} };
 	const Field field2{ Vector2i{980,274},Vector2i{1094,309} };
@@ -37,6 +43,19 @@ int main() {
 		menu.setSize(Vector2f{ 1280,720 });
 		menu.setTexture(&mainMenu);
 		menu.loadField(field1, field2, field3, field4);
+		menu.loadSound(&clickSound);
+
+	Options options;
+		options.setSize(Vector2f{ 1280,720 });
+		options.setTexture(&optionsMenu);
+		options.loadField(field1, field2, field3, field4);
+		options.loadSound(&clickSound);
+
+	Credits credits;
+		credits.setSize(Vector2f{ 1280,720 });
+		credits.setTexture(&creditsMenu);
+		credits.loadField(field1, field2, field3, field4);
+		credits.loadSound(&clickSound);
 
 /////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////
@@ -52,16 +71,49 @@ int main() {
 
 			if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left) {
 				Vector2i mousePosition = Mouse::getPosition(app);
-				if (menu.click(mousePosition) == Type::Options::Start) cout << "Start\n";
-				else if (menu.click(mousePosition) == Type::Options::Settings) cout << "Settings\n";
-				else if (menu.click(mousePosition) == Type::Options::Credits) cout << "Credits\n";
-				else if (menu.click(mousePosition) == Type::Options::Exit) app.close();
+
+				if (menu.open) {
+					if (menu.click(mousePosition) == Type::Options::Start);
+
+					else if (menu.click(mousePosition) == Type::Options::Settings) {
+						menu.open = false;
+						options.open = true;
+					}
+					else if (menu.click(mousePosition) == Type::Options::Credits) {
+						menu.open = false;
+						credits.open = true;
+					}
+
+					else if (menu.click(mousePosition) == Type::Options::Exit) app.close();
+				}
+
+
+				if (options.open) {
+					if (options.click(mousePosition) == Type::Options::FPS);
+
+					else if (options.click(mousePosition) == Type::Options::Volume);
+
+					else if (options.click(mousePosition) == Type::Options::Undo) {
+						menu.open = true;
+						options.open = false;
+					}
+				}
+
+				if (credits.open) {
+
+					if (credits.click(mousePosition) == Type::Options::Undo) {
+						menu.open = true;
+						credits.open = false;
+					}
+				}
 			}
 
 		}
 
 		app.clear();
-			if (menu.open) app.draw(menu);
+			if (menu.open)		app.draw(menu);
+			if (options.open)	app.draw(options);
+			if (credits.open)	app.draw(credits);
 		app.display();
 	}
 	return 0;
