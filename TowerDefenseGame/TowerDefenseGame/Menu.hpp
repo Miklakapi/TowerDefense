5d7f215b4234af7a4c31cac2a34b94bf1afecdd5
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-01-11 22:52:00 +0100

    Update Menu.hpp

--- TowerDefenseGame/TowerDefenseGame/Menu.hpp
+++ TowerDefenseGame/TowerDefenseGame/Menu.hpp
@@ -1,22 +1,40 @@
+#pragma once
 #include <SFML\Window.hpp>
 #include <SFML\System.hpp>
 #include <SFML\Graphics.hpp>
+#include "Type.hpp"
 
 using namespace sf;
 
-class Menu {
+struct Field {
+	Field();
+	Field(Vector2i left, Vector2i right);
+
+	Vector2i leftPosition;
+	Vector2i rightPosition;
+};
+
+class Menu :public RectangleShape{
+
+	Field field1;
+	Field field2;
+	Field field3;
+	Field field4;
 
 public:
 
-	RectangleShape background;
-	RectangleShape start;
-	RectangleShape opcje;
-	RectangleShape autorzy;
-	RectangleShape wyjscie;
+	bool open;
+
+	//
 
 	Menu();
 
-	void click(Vector2i mousePosition);
+	void loadField(Field field1, Field field2, Field field3, Field field4);
 
-	void draw(RenderWindow* window);
+	Type::Options click(Vector2i mousePosition);
+
+};
+
+class Options {
+
 };
