dbcf989b7885e336c59d9b2fe13618edcc0ceac0
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-01-12 17:23:06 +0100

    Update Square.cpp

---
+++ TowerDefenseGame/TowerDefenseGame/Square.cpp
@@ -0,0 +1,24 @@
+#include <SFML\Audio.hpp>
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Graphics.hpp>
+#include "Type.hpp"
+#include "Square.hpp"
+
+using namespace sf;
+
+Square::Square() : RectangleShape() {};
+
+void Square::loadIntRect(IntRect* rect) {
+	this->rect = rect;
+	reset();
+}
+
+void Square::setContent(Type::Content content) {
+	this->content = content;
+	setTextureRect(*(rect + content));
+}
+
+void Square::reset() {
+	setContent(Type::Content::Grass);
+}
