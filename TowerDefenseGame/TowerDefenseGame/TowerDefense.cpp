7f3cc6b2940d816ddd11d22affb420c2433987e7
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-01-13 02:09:39 +0100

    Update TowerDefense.cpp

--- TowerDefenseGame/TowerDefenseGame/TowerDefense.cpp
+++ TowerDefenseGame/TowerDefenseGame/TowerDefense.cpp
@@ -6,6 +6,12 @@
 #include "Type.hpp"
 #include "TowerDefense.hpp"
 
+TowerDefense::TowerDefense() {
+	music = true;
+	round = 1;
+	game = false;
+}
+
 void TowerDefense::setWindow(RenderWindow* window) {
 	this->window = window;
 }
@@ -18,50 +24,105 @@ void TowerDefense::setSound(Sound* sound) {
 }
 
 void TowerDefense::playSound() {
-	if (game == false) {
-		(sound + 1)->stop();
-		sound->play();
+	if (music) {
+		if (game == false) {
+			(sound + round)->stop();
+			sound->play();
+		}
+		else {
+			sound->stop();
+			(sound + round)->play();
+		}
 	}
-	else {
-		sound->stop();
-		(sound + 1)->play();
-	}
+}
+
+void TowerDefense::setOptionsTexture(Texture* texture) {
+	textureOptions = texture;
+	options.settings = 0;
+	options.setTexture((texture));
 }
 
 void TowerDefense::click(Vector2i mousePosition, Mouse::Button button) {
 	if (button == Mouse::Button::Left) {
 		if (menu.open) {
-			if (menu.click(mousePosition) == Type::Options::Start) {
+			if (menu.click(mousePosition, music) == Type::Options::Start) {
 				menu.open = false;
 				game = true;
 				playSound();
 			}
 
-			else if (menu.click(mousePosition) == Type::Options::Settings) {
+			else if (menu.click(mousePosition, music) == Type::Options::Settings) {
 				menu.open = false;
 				options.open = true;
 			}
-			else if (menu.click(mousePosition) == Type::Options::Credits) {
+			else if (menu.click(mousePosition, music) == Type::Options::Credits) {
 				menu.open = false;
 				credits.open = true;
 			}
 
-			else if (menu.click(mousePosition) == Type::Options::Exit) window->close();
+			else if (menu.click(mousePosition, music) == Type::Options::Exit) window->close();
 			return;
 		}
 		if (options.open) {
-			if (options.click(mousePosition) == Type::Options::FPS);
+			if (options.click(mousePosition, music) == Type::Options::FPS) {
+				if (options.settings == 0) {
+					options.settings = 1;
+					options.setTexture(textureOptions + options.settings);
+					window->setFramerateLimit(144);
+				}
+				else if (options.settings == 1) {
+					options.settings = 0;
+					options.setTexture(textureOptions + options.settings);
+					window->setFramerateLimit(60);
+				}
+				else if (options.settings == 2) {
+					options.settings = 3;
+					options.setTexture(textureOptions + options.settings);
+					window->setFramerateLimit(144);
+				}
+				else if (options.settings == 3) {
+					options.settings = 2;
+					options.setTexture(textureOptions + options.settings);
+					window->setFramerateLimit(60);
+				}
+			}
 
-			else if (options.click(mousePosition) == Type::Options::Volume);
+			else if (options.click(mousePosition, music) == Type::Options::Volume) {
+				if (options.settings == 0) {
+					options.settings = 2;
+					options.setTexture(textureOptions + options.settings);
+					music = false;
+					sound->stop();
 
-			else if (options.click(mousePosition) == Type::Options::Undo) {
+				}
+				else if (options.settings == 1) {
+					options.settings = 3;
+					options.setTexture(textureOptions + options.settings);
+					music = false;
+					sound->stop();
+				}
+				else if (options.settings == 2) {
+					options.settings = 0;
+					options.setTexture(textureOptions + options.settings);
+					music = true;
+					sound->play();
+				}
+				else if (options.settings == 3) {
+					options.settings = 1;
+					options.setTexture(textureOptions + options.settings);
+					music = true;
+					sound->play();
+				}
+			}
+
+			else if (options.click(mousePosition, music) == Type::Options::Undo) {
 				menu.open = true;
 				options.open = false;
 			}
 			return;
 		}
 		if (credits.open) {
-			if (credits.click(mousePosition) == Type::Options::Undo) {
+			if (credits.click(mousePosition, music) == Type::Options::Undo) {
 				menu.open = true;
 				credits.open = false;
 			}
