5183aec1c861743d6ccce6ebbd8886e8366cd582
maksym552 <maksym552@gmail.com>
2019-01-13 12:06:29 +0100

    fg

---
+++ HPP/Towers.hpp
@@ -0,0 +1,9 @@
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Graphics.hpp>
+using namespace sf;
+class Tower {
+
+	float deviation(Monsters monster, Tower gun);
+	bool ifdmg(Monsters monster, Tower tower);
+};
