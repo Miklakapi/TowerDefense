22f1f8630e025034f455d2cd2315b9a7ea5511ca
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-01-14 01:06:00 +0100

    Update TowerDefense.cpp

--- TowerDefenseGame/TowerDefenseGame/TowerDefense.cpp
+++ TowerDefenseGame/TowerDefenseGame/TowerDefense.cpp
@@ -7,9 +7,12 @@
 #include "Map.hpp"
 #include "TowerDefense.hpp"
 
-TowerDefense::TowerDefense(RenderWindow* window) : map(window) {
+using namespace sf;
+using namespace std;
+
+TowerDefense::TowerDefense(RenderWindow* window, string file) : map(window), options(window,file) {
 	this->window = window;
-	music = true;
+	if(options.settings == 1 || options.settings == 0)music = true;
 	round = 1;
 	game = false;
 }
@@ -34,12 +37,6 @@ void TowerDefense::playSound() {
 	}
 }
 
-void TowerDefense::setOptionsTexture(Texture* texture) {
-	textureOptions = texture;
-	options.settings = 0;
-	options.setTexture((texture));
-}
-
 void TowerDefense::run() {
 	if (game == true) {
 		fireDude.moveMonster();
@@ -49,77 +46,44 @@ void TowerDefense::run() {
 void TowerDefense::click(Vector2i mousePosition, Mouse::Button button) {
 	if (button == Mouse::Button::Left) {
 		if (menu.open) {
-			if (menu.click(mousePosition, music) == Type::Options::Start) {
+			Type::Options opt = menu.click(mousePosition, music);
+			if (opt == Type::Options::Start) {
 				menu.open = false;
 				game = true;
 				playSound();
 			}
-
-			else if (menu.click(mousePosition, music) == Type::Options::Settings) {
+			else if (opt == Type::Options::Settings) {
 				menu.open = false;
 				options.open = true;
 			}
-			else if (menu.click(mousePosition, music) == Type::Options::Credits) {
+			else if (opt == Type::Options::Credits) {
 				menu.open = false;
 				credits.open = true;
 			}
-
-			else if (menu.click(mousePosition, music) == Type::Options::Exit) window->close();
+			else if (opt == Type::Options::Exit) window->close();
 			return;
-		}
+		}		
 		if (options.open) {
-			if (options.click(mousePosition, music) == Type::Options::FPS) {
+			Type::Options opt = options.click(mousePosition, music);
+			if (opt == Type::Options::Volume) {
 				if (options.settings == 0) {
-					options.settings = 1;
-					options.setTexture(textureOptions + options.settings);
-					window->setFramerateLimit(144);
-				}
-				else if (options.settings == 1) {
-					options.settings = 0;
-					options.setTexture(textureOptions + options.settings);
-					window->setFramerateLimit(60);
-				}
-				else if (options.settings == 2) {
-					options.settings = 3;
-					options.setTexture(textureOptions + options.settings);
-					window->setFramerateLimit(144);
-				}
-				else if (options.settings == 3) {
-					options.settings = 2;
-					options.setTexture(textureOptions + options.settings);
-					window->setFramerateLimit(60);
-				}
-			}
-
-			else if (options.click(mousePosition, music) == Type::Options::Volume) {
-				if (options.settings == 0) {
-					options.settings = 2;
-					options.setTexture(textureOptions + options.settings);
-					music = false;
-					sound->stop();
-
-				}
-				else if (options.settings == 1) {
-					options.settings = 3;
-					options.setTexture(textureOptions + options.settings);
-					music = false;
-					sound->stop();
-				}
-				else if (options.settings == 2) {
-					options.settings = 0;
-					options.setTexture(textureOptions + options.settings);
 					music = true;
 					sound->play();
 				}
-				else if (options.settings == 3) {
-					options.settings = 1;
-					options.setTexture(textureOptions + options.settings);
+				else if (options.settings == 1) {
 					music = true;
 					sound->play();
 				}
+				else if (options.settings == 2) {
+					music = false;
+					sound->stop();
+				}
+				else if (options.settings == 3) {
+					music = false;
+					sound->stop();
+				}
 			}
-
-			else if (options.click(mousePosition, music) == Type::Options::Undo) {
+			else if (opt == Type::Options::Undo) {
 				menu.open = true;
 				options.open = false;
 			}
@@ -134,7 +98,7 @@ void TowerDefense::click(Vector2i mousePosition, Mouse::Button button) {
 		}
 	}
 	else if (button == Mouse::Button::Right) {
-
+		//
 	}
 }
 
