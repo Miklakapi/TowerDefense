64ccd6c721dec3f0d54db56626d2d59ab894f6ca
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-01-15 21:12:38 +0100

    Create LvReader.hpp

---
+++ TowerDefenseGame/TowerDefenseGame/LvReader.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <SFML\Audio.hpp>
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Graphics.hpp>
+#include "Type.hpp"
+#include "Map.hpp"
+
+using namespace sf;
+using namespace std;
+
+class LvReader {
+
+	Map* map;
+
+	Vector2i startPosition;
+
+	Type::Direct* direct;
+
+	int moveNumber;
+
+public:
+
+	LvReader(Map* map);
+
+	void setFile(string file);
+
+	Type::Direct* getDirect();
+
+	int getMoveNumber();
+
+	Vector2i getStartPosition();
+};
