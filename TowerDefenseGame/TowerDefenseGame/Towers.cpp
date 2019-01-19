a0f7a795d7265cca3d0fab75b60fde04b73e27a4
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-01-19 13:54:30 +0100

    Update Towers.cpp

--- TowerDefenseGame/TowerDefenseGame/Towers.cpp
+++ TowerDefenseGame/TowerDefenseGame/Towers.cpp
@@ -4,33 +4,47 @@
 #include <math.h>
 #include "Monsters.hpp"
 #include "Towers.hpp"
-#define PI 3.14159265
 
 using namespace sf;
 
+const double Tower::PI = 3.14159265;
 
 Tower::Tower() : RectangleShape() {
-		setOrigin(Vector2f(40, 40));
-		setSize(Vector2f(80, 80));
+	setOrigin(Vector2f(40, 40));
+	setSize(Vector2f(80, 80));
+}
 
+void Tower::setRange(int range) {
+	this->range = range;
+}
+
+void Tower::setDamage(int damage) {
+	this->damage = damage;
+}
+
+int Tower::getRange() {
+	return range;
+}
+
+void Tower::deviation(Monsters* monster) {
+	if(inRange(monster)) {
+		Vector2f vec = monster->getPosition() - getPosition();
+		float grade = float(atan2(vec.y, vec.x) * 180 / PI);
+		setRotation(grade + 45);
 	}
-	void Tower::deviation(Monsters*monster) {
-		if(ifdmg(monster)) {
-			Vector2f xy = monster->getPosition() - getPosition();
-			float grade = atan2(xy.y, xy.x) * 180 / PI;
-			setRotation(grade + 45);
-		}
+}
+
+bool Tower::inRange(Monsters* monster) {
+	if ((sqrt(((monster->getPosition().x - getPosition().x)*(monster->getPosition().x - getPosition().x)) +
+		((monster->getPosition().y - getPosition().y)*(monster->getPosition().y - getPosition().y)))) <= range) return true;
+	else return false;
+}
+
+int Tower::shoot(Monsters *monster) {
+	if (inRange(monster)) {
+		if (clock.getElapsedTime().asSeconds() < 1) return 0;
+		clock.restart();
+		return damage;
 	}
-	bool Tower::ifdmg(Monsters*monster) {
-		if ((sqrt(((monster->getPosition().x - getPosition().x)*(monster->getPosition().x - getPosition().x)) +
-			((monster->getPosition().y - getPosition().y)*(monster->getPosition().y - getPosition().y)))) < 250) return true;
-		else return false;
-	}
-	int Tower::shoot(Monsters*monster) {
-		if (ifdmg(monster)) {
-			if (clock.getElapsedTime().asSeconds() < 1) return 0;
-			clock.restart();
-			return 40;
-		}
-		else return 0  ;
-	}
+	else return 0  ;
+}
