ba88df936e17180c33517e1f5a80ccc3476efbce
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-01-19 19:40:56 +0100

    Update TowerDefense.cpp

--- TowerDefenseGame/TowerDefenseGame/TowerDefense.cpp
+++ TowerDefenseGame/TowerDefenseGame/TowerDefense.cpp
@@ -11,138 +11,132 @@
 using namespace sf;
 using namespace std;
 
-TowerDefense::TowerDefense(RenderWindow* window, Texture* menuTextures, Texture* optionsTextures, Texture* lv, Texture* monsterTexture,
-	Field* field, Sound* menuMusic, Sound* lvSounds, string settingsFile, string* lvFile,
-	IntRect* lvRect, IntRect* monsterRect, int lvNumbers)
-	: options(window,settingsFile), map(window,lv,lvRect), lvReader(&map), rangeField(Color::Black)  {
-
-	menu.setTexture(menuTextures);
-	credits.setTexture(menuTextures + 1);
-	options.loadTextures(optionsTextures);
-	
-	menu.loadSound(menuMusic);
-	credits.loadSound(menuMusic);
-	options.loadSound(menuMusic);
-	
-	menu.loadField(*(field), *(field + 1), *(field + 2), *(field + 3));
-	credits.loadField(*(field), *(field + 1), *(field + 2), *(field + 3));
-	options.loadField(*(field), *(field + 1), *(field + 2), *(field + 3));
-	
-	lvReader.setFile(*lvFile);
-
-	fireDude.loadIntRect(monsterRect);
-	fireDude.setTexture(monsterTexture);
-	fireDude.setHealth(100);
-	fireDude.setSpeed(10);
-	fireDude.setRoad(lvReader.getDirect(),lvReader.getMoveNumber());
-	fireDude.setPosition(lvReader.getStartPosition());
-
+TowerDefense::TowerDefense(RenderWindow* window, int lvNumbers){
 	this->window = window;
+	game = false;
+	round = 1;
+	this->lvNumbers = lvNumbers;
+}
 
-	this->lvFile = lvFile;
-
-	this->menuMusic = *(menuMusic + 1);
-	this->menuMusic.setLoop(true);
-
-	this->lvSounds = lvSounds;
-	for (int i = 0; i < lvNumbers; i++) {
-		(this->lvSounds + i)->setLoop(true);
-	}
-
-	switch (options.settings) {
+void TowerDefense::loadMenu(Texture* menuTexture, Texture* creditsTexture, Texture* optionsTextures, Field* field, Sound* clickSound, Sound* menuMusic, string optionsFile) {
+	this->menuMusic = menuMusic;
+	menu = new Menu;
+	menu->loadField(*(field), *(field + 1), *(field + 2), *(field + 3));
+		menu->loadSound(clickSound);
+		menu->setTexture(menuTexture);
+	credits = new Credits;
+		credits->loadField(*(field), *(field + 1), *(field + 2), *(field + 3));
+		credits->loadSound(clickSound);
+		credits->setTexture(creditsTexture);
+	options = new Options(window, optionsFile);
+		options->loadField(*(field), *(field + 1), *(field + 2), *(field + 3));
+		options->loadSound(clickSound);
+		options->loadTextures(optionsTextures);
+	switch (options->settings) {
 	case 0:
-		window->setFramerateLimit(60);
 		music = true;
 		break;
 	case 1:
-		window->setFramerateLimit(144);
 		music = true;
 		break;
 	case 2:
-		window->setFramerateLimit(60);
 		music = false;
 		break;
 	case 3:
-		window->setFramerateLimit(144);
 		music = false;
 		break;
 	}
-
-	game = false;
-
-	round = 1;
-	
-	this->lvNumbers = lvNumbers;
-
-	playSound();
+	if (music) this->menuMusic->play();
 }
+
+void TowerDefense::loadMap(Texture* mapTextures, IntRect* rect, Sound* lvSounds) {
+	this->lvSounds = lvSounds;
+	map = new Map(window, mapTextures, rect);
+}
+
+void TowerDefense::loadLvReader(string* lvFiles) {
+	this->lvFiles = lvFiles;
+	lvReader = new LvReader(map);
+	lvReader->setFile(*lvFiles);
+}
+
+void TowerDefense::loadRangeField() {
+	rangeField = new RangeField(Color{ 0, 0, 255, 50 });
+	rangeField->turnOff();
+}
+
+void TowerDefense::loadPointCounter(Font& font) {
+	pointCounter = new PointCounter(font);
+}
+
+//
 
 void TowerDefense::playSound() {
 	if (!game) {
 		(lvSounds + (round - 1))->stop();
-		if (music) menuMusic.play();
-		else menuMusic.stop();
+		if (music) menuMusic->play();
+		else menuMusic->stop();
 	}
 	else {
-		menuMusic.stop();
+		menuMusic->stop();
 		if (music) (lvSounds + (round - 1))->play();
 		else (lvSounds + (round - 1))->stop();
 	}
 }
 
+//Public:
+
 void TowerDefense::run() {
-	if (game == true) {
-		fireDude.moveMonster();
-	}
+
 }
 
 void TowerDefense::click(Vector2i mousePosition, Mouse::Button button) {
 	if (button == Mouse::Button::Left) {
-		if (menu.open) {
-			Type::Options opt = menu.click(mousePosition, music);
+		if (menu->open) {
+			Type::Options opt = menu->click(mousePosition, music);
 			if (opt == Type::Options::Start) {
-				menu.open = false;
+				menu->open = false;
 				game = true;
 				playSound();
 			}
 			else if (opt == Type::Options::Settings) {
-				menu.open = false;
-				options.open = true;
+				menu->open = false;
+				options->open = true;
 			}
 			else if (opt == Type::Options::Credits) {
-				menu.open = false;
-				credits.open = true;
+				menu->open = false;
+				credits->open = true;
 			}
 			else if (opt == Type::Options::Exit) window->close();
 			return;
 		}
-		if (options.open) {
-			Type::Options opt = options.click(mousePosition, music);
+		if (options->open) {
+			Type::Options opt = options->click(mousePosition, music);
 			if (opt == Type::Options::Volume) {
-				if (options.settings == 0) {
+				if (options->settings == 0) {
 					music = true;
 				}
-				else if (options.settings == 1) {
+				else if (options->settings == 1) {
 					music = true;
 				}
-				else if (options.settings == 2) {
+				else if (options->settings == 2) {
 					music = false;
 				}
-				else if (options.settings == 3) {
+				else if (options->settings == 3) {
 					music = false;
 				}
 				playSound();
 			}
 			else if (opt == Type::Options::Undo) {
-				menu.open = true;
-				options.open = false;
+				menu->open = true;
+				options->open = false;
 			}
 			return;
 		}
-		if (credits.open) {
-			if (credits.click(mousePosition, music) == Type::Options::Undo) {
-				menu.open = true;
-				credits.open = false;
+		if (credits->open) {
+			if (credits->click(mousePosition, music) == Type::Options::Undo) {
+				menu->open = true;
+				credits->open = false;
 			}
 			return;
 		}
@@ -153,11 +147,11 @@ void TowerDefense::click(Vector2i mousePosition, Mouse::Button button) {
 }
 
 void TowerDefense::drawAll() {
-	if (menu.open)window->draw(menu);
-	else if (options.open)window->draw(options);
-	else if (credits.open)window->draw(credits);
+	if (menu->open)window->draw(*menu);
+	else if (options->open)window->draw(*options);
+	else if (credits->open)window->draw(*credits);
 	else if (game) {
-		map.drawAll();
-		if (fireDude.isLive()) window->draw(fireDude);
+		map->drawAll();
+		//if (fireDude->isLive()) window->draw(*fireDude);
 	}
 }
