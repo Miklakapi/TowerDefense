736e764be65d2ecb0ac034a9e293c3c6f7ad8b87
Kacper Miklaszewski <45365372+Miklakapi@users.noreply.github.com>
2019-01-23 23:52:19 +0100

    Delete Towers.hpp

--- HPP/Towers.hpp
+++
@@ -1,9 +0,0 @@
-#include <SFML\Window.hpp>
-#include <SFML\System.hpp>
-#include <SFML\Graphics.hpp>
-using namespace sf;
-class Tower {
-
-	float deviation(Monsters monster, Tower gun);
-	bool ifdmg(Monsters monster, Tower tower);
-};
